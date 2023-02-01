996643a13d68069dde1b620b0edc0e20ca9c1782
Daniel H Gonzalez <raptor3500@hotmail.com>
2023-02-01 12:11:30 -0500

    good attempt

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-test
+import json
+
+
+with open ("imdb_movies_1985to2022.json", "r") as in_file:
+    
+    for line in in_file:
+        print(line)
+        this_movie = json.loads(line)
+        
+        actors = this_movie["actors"]
+        for actor in actors:
+            actor_name = actor[0]
+            if actor_name == ['nm0413168', 'Hugh Jackman']:
+                print("/t",actor_name)
+                
+
